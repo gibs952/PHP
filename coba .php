7be26683d9d589b6ddb942db34cab786a58e0ae5
gibs952 <wildankhairusunny@gmail.com>
2022-12-15 09:23:59 +0700

    belajar PHP pertemuan 10

---
+++ coba .php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>apakek</title>
+</head>
+
+<body>
+    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic incidunt, doloremque expedita fugiat ut vero. Soluta a, explicabo vero aut autem inventore reiciendis dolore voluptas fugiat consequatur molestias eum id.
+        
+    </h1>
+
+    <?php
+$txt = "PHP";
+echo "I love $txt!";
+?>
+</body>
+
+</html>
